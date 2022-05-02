6b5c3039ae82f70de45a1c28fc9bc972932d1e04
leoniro <leoniro@gmail.com>
2022-05-02 01:27:53 -0300

    UI keyboard now lights up with output

--- sketch.js
+++ sketch.js
@@ -1,88 +1,15 @@
-var rotorCiphers = ['EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
-  'BDFHJLCPRTXVZNYEIWGAKMUSQO', 'ESOVPZJAYQUIRHXLNFTGKDCMWB', 'VZBRGITYUPSDNHLXAWMJQOFECK'];
-var rotorNotches = [17, 5, 22, 10, 0];
-var reflectorCiphers = ['EJMZALYXVBWFCRQUONTSPIKHGD','YRUHQSLDPXNGOKMIEBFZCWVJAT',
-    'FVPJIAOYEDRZXWGCTKUQSBNMHL'];
-
-function setup() {
-  createCanvas(470,470)
-  background(51)
-  var keyboard = new Keyboard(100,150,'original');
-  keyboard.draw();
-
-  let plugboard = new Plugboard(reflectorCiphers[0]);
-  let rotor0 = new Rotor(rotorCiphers[0],rotorNotches[0]);
-  let rotor1 = new Rotor(rotorCiphers[1],rotorNotches[1]);
-  let rotor2 = new Rotor(rotorCiphers[2],rotorNotches[2]);
-  let reflector = new Rotor(reflectorCiphers[1],'');
-  let rotorList = [rotor0, rotor1, rotor2, reflector];
-  let rotorArray = new RotorArray(rotorList,[0, 0, 0]);
-
-  for (let k = 0; k<=10; k++) {
-    console.log(decode(k,plugboard,rotorArray));
-  }
-}
-
-function draw() {
-  // put drawing code here
-  noLoop();
-}
-
-function decode(n,plugboard,rotorArray) {
-  // keystroke goes through plugboard
-  let out = plugboard.swap(n);
-
-  // then traverses the rotor array wiring
-  for (let k=0; k<rotorArray.state.length; k++) {
-    out = rotorArray.rotors[k].fwd(out,rotorArray.state[k]);
-  }
-
-  // bounces back through the reflector (bwd and fwd methods should give the same result)
-  out = rotorArray.rotors[rotorArray.state.length].fwd(out);
-
-  // traversers rotor array in reverse
-  for (let k = rotorArray.state.length - 1; k>=0; k--) {
-    out = rotorArray.rotors[k].bwd(out,rotorArray.state[k]);
-  }
-
-  // finally thorugh the plugboard again
-  out = plugboard.swap(out);
-
-  // advances rotor array state
-  rotorArray.click();
-  return out;
-}
-
-function letter2num(letter) {
-  return parseInt(letter,36) - 10;
-}
-
-function num2letter(n) {
-  letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  return letters[n]
-}
-
-function reverseCipher(cipher) {
-  let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  let cipherR = new Array(26);
-  for (let k = 0; k < 26; k++) {
-    cipherR[letter2num(cipher[k])] = letters[k];
-  }
-  return cipherR.reduce( (x,y) => x + y );
-}
-
 class Key {
   constructor(idx,layout,r=15) {
-    let letters; let keysPerRow;
+    let Layoutletters; let keysPerRow;
     if (layout == 'original') {
-      letters = 'QWERTZUIOASDFGHJKPYZXCVBNML'
+      Layoutletters = 'QWERTZUIOASDFGHJKPYXCVBNML'
       keysPerRow = [9, 8, 9];
     }
     else {
-      letters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+      Layoutletters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
       keysPerRow = [10, 9, 7];
     }
-    this.label = letters[idx]
+    this.label = Layoutletters[idx]
     this.r = r;
     let row; let col;
     if (idx < keysPerRow[0]) { 
@@ -104,9 +31,11 @@ class Key {
     }
   }
   draw(x0,y0) {
+    fill('white')
     ellipse(this.xRelative + x0, this.yRelative +y0, 2*this.r, 2*this.r)
     textSize(this.r)
     textAlign(CENTER, CENTER)
+    fill('black')
     text(this.label, this.xRelative + x0,this.yRelative + y0)
   }
 }
@@ -116,14 +45,27 @@ class Keyboard {
     this.xpos = x0;
     this.ypos = y0;
     this.key = new Array(26);
+    this.key[25] = 0;
+    var key;
     for (let k=0; k<26; k++) {
-      this.key[k] = new Key(k, layout);
+      key = new Key(k, layout);
+      this.key[letter2num(key.label)] = key;
     }
   }
   draw() {
     for (let k=0; k<26; k++) {
       this.key[k].draw(this.xpos,this.ypos);
     }
+  }
+  highlightKey(n) {
+    this.draw();
+    let x, y, r;
+    x = this.key[n].xRelative + this.xpos;
+    y = this.key[n].yRelative + this.ypos;
+    r = this.key[n].r;
+    fill(10,150,10,150)
+    ellipse(x,y,2*r,2*r);
+    noFill();
   }
 }
 
@@ -143,20 +85,6 @@ class Rotor {
   }
 }
 
-function rotateCipher(cipher,state) {
-  let wiring = new Array(52);
-  let out = [];
-  for (let k=0; k<26; k++) {
-    wiring[k] = (parseInt(cipher[k],36) - k + 26 - 10) % 26;
-    wiring[k+26] = wiring[k];
-    out.push(k);
-  }
-  
-  for (let k=0; k<26; k++) {
-    out[k] = (out[k] + wiring[state+k]) % 26;
-  }
-  return out.map(num2letter).reduce( (x,y) => x+y );
-}
 
 class RotorArray {
   // The reflector works like a symmetric rotor, and should be the last (usually 4th) Rotor in the list
@@ -185,4 +113,99 @@ class Plugboard {
   swap(n) { // swap letters if they are connected
     return letter2num(this.cipher[n]);
   }
+}
+
+var rotorCiphers = ['EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
+  'BDFHJLCPRTXVZNYEIWGAKMUSQO', 'ESOVPZJAYQUIRHXLNFTGKDCMWB', 'VZBRGITYUPSDNHLXAWMJQOFECK'];
+var rotorNotches = [17, 5, 22, 10, 0];
+var reflectorCiphers = ['EJMZALYXVBWFCRQUONTSPIKHGD','YRUHQSLDPXNGOKMIEBFZCWVJAT',
+    'FVPJIAOYEDRZXWGCTKUQSBNMHL'];
+
+var plugboard = new Plugboard(reflectorCiphers[0]);
+var rotor0 = new Rotor(rotorCiphers[0],rotorNotches[0]);
+var rotor1 = new Rotor(rotorCiphers[1],rotorNotches[1]);
+var rotor2 = new Rotor(rotorCiphers[2],rotorNotches[2]);
+var reflector = new Rotor(reflectorCiphers[1],'');
+var rotorList = [rotor0, rotor1, rotor2, reflector];
+var rotorArray = new RotorArray(rotorList,[0, 0, 0]);
+
+var keyboard = new Keyboard(100,150,'original');
+
+
+function setup() {
+  createCanvas(470,470)
+  background(51)
+  keyboard.draw();
+}
+
+function draw() {
+  // noLoop();
+}
+
+function decode(n,plugboard,rotorArray) {
+  // keystroke goes through plugboard
+  let out = plugboard.swap(n);
+
+  // then traverses the rotor array wiring
+  for (let k=0; k<rotorArray.state.length; k++) {
+    out = rotorArray.rotors[k].fwd(out,rotorArray.state[k]);
+  }
+
+  // bounces back through the reflector (bwd and fwd methods should give the same result)
+  out = rotorArray.rotors[rotorArray.state.length].fwd(out);
+
+  // traversers rotor array in reverse
+  for (let k = rotorArray.state.length - 1; k>=0; k--) {
+    out = rotorArray.rotors[k].bwd(out,rotorArray.state[k]);
+  }
+
+  // finally thorugh the plugboard again
+  out = plugboard.swap(out);
+
+  // advances rotor array state
+  rotorArray.click();
+  return out;
+}
+
+function keyPressed() {
+  let n = parseInt(key,36) - 10;
+  if ( (n>=0) && (n<=25) ) {
+    n = decode(n,plugboard,rotorArray);
+    console.log(num2letter(n));
+    keyboard.highlightKey(n);
+  }
+}
+
+function letter2num(letter) {
+  return parseInt(letter,36) - 10;
+}
+
+function num2letter(n) {
+  letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return letters[n]
+}
+
+function reverseCipher(cipher) {
+  let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let cipherR = new Array(26);
+  for (let k = 0; k < 26; k++) {
+    cipherR[letter2num(cipher[k])] = letters[k];
+  }
+  return cipherR.reduce( (x,y) => x + y );
+}
+
+
+function rotateCipher(cipher,state) {
+  let wiring = new Array(52);
+  let out = [];
+  for (let k=0; k<26; k++) {
+    wiring[k] = (parseInt(cipher[k],36) - k + 26 - 10) % 26;
+    wiring[k+26] = wiring[k];
+    out.push(k);
+  }
+  
+  for (let k=0; k<26; k++) {
+    out[k] = (out[k] + wiring[state+k]) % 26;
+  }
+  return out.map(num2letter).reduce( (x,y) => x+y );
 }
